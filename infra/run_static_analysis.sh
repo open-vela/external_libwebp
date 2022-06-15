aaa195e4649204972d538e2391b9427728dd01ae
openvela-robot <openvela-robot@xiaomi.com>
2022-06-16 05:11:26 +0800

    Merge "add WEBP_MSAN" into main

--- infra/run_static_analysis.sh
+++ infra/run_static_analysis.sh
@@ -85,10 +85,10 @@ scan_build make -j4
 
 index="$(find "${OUTPUT_DIR}" -name index.html)"
 if [[ -f "${index}" ]]; then
-  mv "$(dirname "${index}")/"* "${OUTPUT_DIR}"
+  mv "$(dirname "${index}")/"* .
 else
   # make a empty report to wipe out any old bug reports.
-  cat << EOT > "${OUTPUT_DIR}/index.html"
+  cat << EOT > index.html
 <html>
 <body>
 No bugs reported.
