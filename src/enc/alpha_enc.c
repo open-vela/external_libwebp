90fd1762813dca05a86b79350365ee03483b7fa8
liujinye <liujinye@xiaomi.com>
2023-02-24 01:29:40 +0800

    Merge "Update yapf style from "chromium" to "yapf"" into main

--- src/enc/alpha_enc.c
+++ src/enc/alpha_enc.c
@@ -13,7 +13,6 @@
 
 #include <assert.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "src/enc/vp8i_enc.h"
 #include "src/dsp/dsp.h"
@@ -149,7 +148,6 @@ static int EncodeAlphaInternal(const uint8_t* const data, int width, int height,
       }
     } else {
       VP8LBitWriterWipeOut(&tmp_bw);
-      memset(&result->bw, 0, sizeof(result->bw));
       return 0;
     }
   }
@@ -164,7 +162,7 @@ static int EncodeAlphaInternal(const uint8_t* const data, int width, int height,
   header = method | (filter << 2);
   if (reduce_levels) header |= ALPHA_PREPROCESSED_LEVELS << 4;
 
-  if (!VP8BitWriterInit(&result->bw, ALPHA_HEADER_LEN + output_size)) ok = 0;
+  VP8BitWriterInit(&result->bw, ALPHA_HEADER_LEN + output_size);
   ok = ok && VP8BitWriterAppend(&result->bw, &header, ALPHA_HEADER_LEN);
   ok = ok && VP8BitWriterAppend(&result->bw, output, output_size);
 
