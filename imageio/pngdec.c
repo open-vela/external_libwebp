84edd369e969dd5c6a37ab2f8f4fac1e2d1b4071
openvela-robot <openvela-robot@xiaomi.com>
2023-06-22 16:35:32 +0800

    webp-lossless-bitstream-spec: update transformations text
    follow up to:
    29c9f2d4 webp-lossless-bitstream-spec: minor wording updates
    
    Universally use 'transform' over 'transformation'. The doc generally
    uses 'transform' to talk about the individual transforms themselves as
    well as their related data.
    
    Bug: webp:611
    Change-Id: I097701873bbc440bd399ab5440a914a14f7c62f6

--- imageio/pngdec.c
+++ imageio/pngdec.c
@@ -235,7 +235,7 @@ int ReadPNG(const uint8_t* const data, size_t data_size,
   volatile png_infop end_info = NULL;
   PNGReadContext context = { NULL, 0, 0 };
   int color_type, bit_depth, interlaced;
-  int num_channels;
+  int has_alpha;
   int num_passes;
   int p;
   volatile int ok = 0;
@@ -293,6 +293,9 @@ int ReadPNG(const uint8_t* const data, size_t data_size,
   }
   if (png_get_valid(png, info, PNG_INFO_tRNS)) {
     png_set_tRNS_to_alpha(png);
+    has_alpha = 1;
+  } else {
+    has_alpha = !!(color_type & PNG_COLOR_MASK_ALPHA);
   }
 
   // Apply gamma correction if needed.
@@ -307,16 +310,13 @@ int ReadPNG(const uint8_t* const data, size_t data_size,
 
   if (!keep_alpha) {
     png_set_strip_alpha(png);
+    has_alpha = 0;
   }
 
   num_passes = png_set_interlace_handling(png);
   png_read_update_info(png, info);
 
-  num_channels = png_get_channels(png, info);
-  if (num_channels != 3 && num_channels != 4) {
-    goto Error;
-  }
-  stride = (int64_t)num_channels * width * sizeof(*rgb);
+  stride = (int64_t)(has_alpha ? 4 : 3) * width * sizeof(*rgb);
   if (stride != (int)stride ||
       !ImgIoUtilCheckSizeArgumentsOverflow(stride, height)) {
     goto Error;
@@ -341,8 +341,8 @@ int ReadPNG(const uint8_t* const data, size_t data_size,
 
   pic->width = (int)width;
   pic->height = (int)height;
-  ok = (num_channels == 4) ? WebPPictureImportRGBA(pic, rgb, (int)stride)
-                           : WebPPictureImportRGB(pic, rgb, (int)stride);
+  ok = has_alpha ? WebPPictureImportRGBA(pic, rgb, (int)stride)
+                 : WebPPictureImportRGB(pic, rgb, (int)stride);
 
   if (!ok) {
     goto Error;
--- src/enc/picture_rescale_enc.c
+++ src/enc/picture_rescale_enc.c
@@ -137,9 +137,7 @@ int WebPPictureCrop(WebPPicture* pic,
   PictureGrabSpecs(pic, &tmp);
   tmp.width = width;
   tmp.height = height;
-  if (!WebPPictureAlloc(&tmp)) {
-    return WebPEncodingSetError(pic, tmp.error_code);
-  }
+  if (!WebPPictureAlloc(&tmp)) return 0;
 
   if (!pic->use_argb) {
     const int y_offset = top * pic->y_stride + left;
@@ -214,28 +212,26 @@ int WebPPictureRescale(WebPPicture* picture, int width, int height) {
   prev_height = picture->height;
   if (!WebPRescalerGetScaledDimensions(
           prev_width, prev_height, &width, &height)) {
-    return WebPEncodingSetError(picture, VP8_ENC_ERROR_BAD_DIMENSION);
+    return 0;
   }
 
   PictureGrabSpecs(picture, &tmp);
   tmp.width = width;
   tmp.height = height;
-  if (!WebPPictureAlloc(&tmp)) {
-    return WebPEncodingSetError(picture, tmp.error_code);
-  }
+  if (!WebPPictureAlloc(&tmp)) return 0;
 
   if (!picture->use_argb) {
     work = (rescaler_t*)WebPSafeMalloc(2ULL * width, sizeof(*work));
     if (work == NULL) {
       WebPPictureFree(&tmp);
-      return WebPEncodingSetError(picture, VP8_ENC_ERROR_OUT_OF_MEMORY);
+      return 0;
     }
     // If present, we need to rescale alpha first (for AlphaMultiplyY).
     if (picture->a != NULL) {
       WebPInitAlphaProcessing();
       if (!RescalePlane(picture->a, prev_width, prev_height, picture->a_stride,
                         tmp.a, width, height, tmp.a_stride, work, 1)) {
-        return WebPEncodingSetError(picture, VP8_ENC_ERROR_BAD_DIMENSION);
+        return 0;
       }
     }
 
@@ -250,14 +246,14 @@ int WebPPictureRescale(WebPPicture* picture, int width, int height) {
         !RescalePlane(picture->v, HALVE(prev_width), HALVE(prev_height),
                       picture->uv_stride, tmp.v, HALVE(width), HALVE(height),
                       tmp.uv_stride, work, 1)) {
-      return WebPEncodingSetError(picture, VP8_ENC_ERROR_BAD_DIMENSION);
+      return 0;
     }
     AlphaMultiplyY(&tmp, 1);
   } else {
     work = (rescaler_t*)WebPSafeMalloc(2ULL * width * 4, sizeof(*work));
     if (work == NULL) {
       WebPPictureFree(&tmp);
-      return WebPEncodingSetError(picture, VP8_ENC_ERROR_OUT_OF_MEMORY);
+      return 0;
     }
     // In order to correctly interpolate colors, we need to apply the alpha
     // weighting first (black-matting), scale the RGB values, and remove
@@ -267,7 +263,7 @@ int WebPPictureRescale(WebPPicture* picture, int width, int height) {
     if (!RescalePlane((const uint8_t*)picture->argb, prev_width, prev_height,
                       picture->argb_stride * 4, (uint8_t*)tmp.argb, width,
                       height, tmp.argb_stride * 4, work, 4)) {
-      return WebPEncodingSetError(picture, VP8_ENC_ERROR_BAD_DIMENSION);
+      return 0;
     }
     AlphaMultiplyARGB(&tmp, 1);
   }
